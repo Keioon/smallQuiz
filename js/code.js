9d4301591ff5337fe4333a907f06b43a646629cf
Keioon <78851196+Keioon@users.noreply.github.com>
2021-07-21 20:26:13 +0200

    Changed progresBar functioning

--- js/code.js
+++ js/code.js
@@ -1,12 +1,22 @@
 const nextButton = document.querySelector('.nextQuestion');
+let progress = 0;
+
+function progressAktualization(progress) {
+  document.querySelector(".container p").innerHTML = `${progress}% progress`;
+  document.querySelector(".progressBar progress").style.width = `${progress}%`;
+};
+
 
 nextButton.addEventListener('click', function(){
-  console.log(dataSources.questions);
+  progress += 8;
+  if(progress > 100){
+    progress = 100;
+  }
+  progressAktualization(progress);
 });
 
 const answerButtons = document.querySelectorAll('.answer');
 
-console.log(answerButtons);
 for(let i=0; i<answerButtons.length; i++) {
   answerButtons[i].addEventListener('click', function () {
     answerButtons[i].classList.toggle('choosen');
