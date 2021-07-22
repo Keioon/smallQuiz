a6168730389f01fcc99cdd2235ca87b0a32e5665
Keioon <78851196+Keioon@users.noreply.github.com>
2021-07-22 08:49:20 +0200

    Added dateJs and nextButton function

--- js/code.js
+++ js/code.js
@@ -1,18 +1,37 @@
 const nextButton = document.querySelector('.nextQuestion');
 let progress = 0;
+let idOfQuestion = 0;
 
 function progressAktualization(progress) {
   document.querySelector(".container p").innerHTML = `${progress}% progress`;
   document.querySelector(".progressBar progress").style.width = `${progress}%`;
 };
 
+function changingQuestionOnNext(id) {
+  console.log(document.querySelector('.questionBox h3'));
+  document.querySelector('.questionBox h3').innerHTML = dataSources.questions[id].txt;
+  document.querySelector('.questionBox pre').innerHTML = dataSources.questions[id].pre;
+  document.querySelector('ul li:nth-of-type(1) button').innerHTML = dataSources.questions[id].answers.a;
+  document.querySelector('ul li:nth-of-type(2) button').innerHTML = dataSources.questions[id].answers.b;
+  document.querySelector('ul li:nth-of-type(3) button').innerHTML = dataSources.questions[id].answers.c;
+  document.querySelector('ul li:nth-of-type(4) button').innerHTML = dataSources.questions[id].answers.d;
+}
+
+function removingClassChoosen() {
+  for(let i=0; i<answerButtons.length; i++) {
+    answerButtons[i].classList.remove("choosen");
+  }
+}
 
 nextButton.addEventListener('click', function(){
-  progress += 8;
+  progress += 10;
   if(progress > 100){
     progress = 100;
   }
   progressAktualization(progress);
+  changingQuestionOnNext(idOfQuestion);
+  idOfQuestion ++;
+  removingClassChoosen();
 });
 
 const answerButtons = document.querySelectorAll('.answer');
--- js/data.js
+++ js/data.js
@@ -2,19 +2,21 @@ const dataSources = {};  //object in wchih I have array named Question
 
 dataSources.questions = [
   {
-    id: 1,
-    question: 'What is the correct HTML for referring to an external style sheet?',
+    id: '0',
+    txt: 'What is the correct HTML for referring to an external style sheet?',
+    pre: " ",
     answers: {
-      a: '<style src="style.css">',
-      b: '<stylesheet>style.css</stylesheet>',
-      c: '<style href="style.css">',
-      d: '<link rel="stylesheet" href="css/style.css">'
+      a: '&lt;style src="style.css"&gt;&lt;/style&gt;', 
+      b: '&lt;stylesheet&gt;style.css&lt;/stylesheet&gt;',
+      c: '&lt;style href="style.css"&gt;&lt;/style&gt;',
+      d: '&lt;link rel="stylesheet" href="css/style.css"&gt;'
     },
     correctAnswer: 'd'
   },
   {
-    id: 2,
-    question: 'What does CSS stand for?',
+    id: '1',
+    txt: 'What does CSS stand for?',
+    pre: " ",
     answers: {
       a: 'Creative Style Sheet',
       b: 'Colorful Style Sheet',
@@ -24,8 +26,9 @@ dataSources.questions = [
     correctAnswer: 'c'
   },
   {
-    id: 3,
-    question: 'What does HTML stand for?',
+    id: '2',
+    txt: 'What does HTML stand for?',
+    pre: " ",
     answers: {
       a: 'Hyperlinks and Text Markup Language',
       b: 'Home Tool Markup Language',
@@ -33,5 +36,89 @@ dataSources.questions = [
       d: 'Home Text Mixed Language'
     },
     correctAnswer: 'c'
+  },
+  {
+    id: '3',
+    txt: 'Who is making the Web standards?',
+    pre: " ",
+    answers: {
+      a: 'The World Wide Web Consortium',
+      b: 'Google',
+      c: 'Microsoft',
+      d: 'Mozilla'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    id: '4',
+    txt: 'Which character is used to indicate an end tag?',
+    pre: " ",
+    answers: {
+      a: '<',
+      b: '*',
+      c: '^',
+      d: '/'
+    },
+    correctAnswer: 'd'
+  },
+  {
+    id: '5',
+    txt: 'Which HTML attribute is used to define inline styles?',
+    pre: " ",
+    answers: {
+      a: 'class',
+      b: 'styles',
+      c: 'style',
+      d: 'font'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    id: '6',
+    txt: 'Inline elements are normally displayed without starting a new line.',
+    pre: " ",
+    answers: {
+      a: 'False',
+      b: 'True',
+      c: 'In Html we dont have inline elements',
+      d: 'Inline elements can not change font size'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    id: '7',
+    txt: 'Which input type defines a slider control?',
+    pre: " ",
+    answers: {
+      a: 'range',
+      b: 'controls',
+      c: 'slider',
+      d: 'search'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    id: '8',
+    txt: 'In HTML, which attribute is used to specify that an input field must be filled out?',
+    pre: " ",
+    answers: {
+      a: 'placeholder',
+      b: 'formvalidate',
+      c: 'required',
+      d: 'validate'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    id: '9',
+    txt: 'Which HTML attribute specifies an alternate text for an image, if the image cannot be displayed?',
+    pre: " ",
+    answers: {
+      a: 'title',
+      b: 'src',
+      c: 'longdesc',
+      d: 'alt'
+    },
+    correctAnswer: 'd'
   },
 ];
